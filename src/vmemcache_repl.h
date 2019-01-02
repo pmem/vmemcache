bcead100a8cf625684770d6c6ba7327447d53d6a
Lukasz Dorau <lukasz.dorau@intel.com>
2019-01-02 12:56:46 +0100

    replace if with assert in repl_p_lru_use()

--- src/vmemcache_repl.h
+++ src/vmemcache_repl.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018, Intel Corporation
+ * Copyright 2018-2019, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -66,7 +66,7 @@ struct repl_p_ops {
 					struct repl_p_entry *entry);
 
 	/* use the element */
-	int
+	void
 		(*repl_p_use)(struct repl_p_head *head,
 					struct repl_p_entry *entry);
 };
