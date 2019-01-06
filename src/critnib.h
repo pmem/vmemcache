40cfeeac76bb11d6a0362845efbe91aad55ef468
Adam Borowski <adam.borowski@intel.com>
2019-01-06 05:11:30 +0100

    Add a missing include guard.

--- src/critnib.h
+++ src/critnib.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018, Intel Corporation
+ * Copyright 2018-2019, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,6 +30,9 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#ifndef CRITNIB_H
+#define CRITNIB_H
+
 #include "vmemcache.h"
 
 struct critnib;
@@ -40,3 +43,5 @@ void critnib_delete(struct critnib *c);
 int critnib_set(struct critnib *c, struct cache_entry *e);
 void *critnib_get(struct critnib *c, const struct cache_entry *e);
 void *critnib_remove(struct critnib *c, const struct cache_entry *e);
+
+#endif
