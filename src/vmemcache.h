aad3c03af4abcb775becb9f1fa2aa50fd723ec45
Lukasz Dorau <lukasz.dorau@intel.com>
2019-01-25 11:30:23 +0100

    replace 'struct repl_p repl' with 'struct repl_p *repl'
    ... in the 'struct vmemcache' in preparation to add a 'free' callback
    to vmcache_index_delete() and critnib_delete().

--- src/vmemcache.h
+++ src/vmemcache.h
@@ -40,7 +40,7 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#include "vmemcache_repl.h"
+#include "libvmemcache.h"
 #include "sys_util.h"
 #include "vec.h"
 
@@ -56,13 +56,14 @@ extern "C" {
 typedef unsigned long long stat_t;
 
 struct index;
+struct repl_p;
 
 struct vmemcache {
 	void *addr;			/* mapping address */
 	size_t size;			/* mapping size */
 	struct heap *heap;		/* heap address */
 	struct index *index;		/* indexing structure */
-	struct repl_p repl;		/* replacement policy abstraction */
+	struct repl_p *repl;		/* replacement policy abstraction */
 	vmemcache_on_evict *on_evict;	/* callback on evict */
 	void *arg_evict;		/* argument for callback on evict */
 	vmemcache_on_miss *on_miss;	/* callback on miss */
