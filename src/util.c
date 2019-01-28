ceeeeeb29e11570c4ec20b4cc9e477abee84a591
Adam Borowski <adam.borowski@intel.com>
2019-01-28 13:51:09 +0100

    A debug function for printing a chunk of near-string memory.
    By "near-string" I mean data that's supposed to be mostly text, with
    non-printables being a minority.  No Unicode support (would need to
    validate something that might be random junk).

--- src/util.c
+++ src/util.c
@@ -449,6 +449,35 @@ util_readline(FILE *fh)
 
 	return buffer;
 }
+
+/*
+ * util_print_mem -- print a near-string piece of memory, escaping non-text
+ */
+void
+util_print_mem(const char *s, size_t len)
+{
+	for (; len > 0; len--, s++) {
+		switch (*s) {
+		case 0:
+			printf("\\0"); break;
+		case '\b':
+			printf("\\b"); break;
+		case '\e':
+			printf("\\e"); break;
+		case '\n':
+			printf("\\n"); break;
+		case '\r':
+			printf("\\r"); break;
+		case '\\':
+			printf("\\\\"); break;
+		case 0x7f:
+			printf("\\x7f"); break;
+		default:
+			printf((*s >= ' ' && *s < 0x7f) ? "%c" : "\\x%02x", *s);
+		}
+	}
+}
+
 #endif
 
 /*
