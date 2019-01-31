44e291e447d7776301114f17b30188f53d112f54
Adam Borowski <adam.borowski@intel.com>
2019-01-31 17:17:31 +0100

    simul: insert more than a single byte.
    max_size for now.
    
    Because we want to allow storing massive pieces of data (I've seen people
    putting VM images into SQL...), mallocing the buffer to insert is a bad
    idea, mmap zeroes instead.

--- benchmarks/bench_simul.c
+++ benchmarks/bench_simul.c
@@ -42,12 +42,14 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/mman.h>
 
 #include "libvmemcache.h"
 #include "test_helpers.h"
 #include "os_thread.h"
 #include "benchmark_time.h"
 #include "rand.h"
+#include "util.h"
 
 #define PROG "bench_simul"
 #define MAX_THREADS 4096
@@ -77,6 +79,7 @@ static uint64_t junk_start = 0;
 
 static VMEMcache *cache;
 static int cache_is_full = 0;
+static const void *lotta_zeroes;
 
 /* case insensitive */
 static const char *enum_repl[] = {
@@ -271,8 +274,8 @@ static void *worker(void *arg)
 		char val[1];
 		if (vmemcache_get(cache, key, key_size, val, sizeof(val), 0,
 			NULL) <= 0) {
-			if (vmemcache_put(cache, key, key_size, val, 1) &&
-				errno != EEXIST) {
+			if (vmemcache_put(cache, key, key_size, lotta_zeroes,
+				max_size) && errno != EEXIST) {
 				UT_FATAL("vmemcache_put failed");
 			}
 		}
@@ -423,6 +426,13 @@ main(int argc, const char **argv)
 		} else
 			print_units(*p->var);
 		printf("\n");
+	}
+
+	lotta_zeroes = mmap(NULL, max_size, PROT_READ,
+		MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE, -1, 0);
+	if (!lotta_zeroes) {
+		UT_FATAL("couldn't grab a zero buffer: mmap failed: %s",
+			strerror(errno));
 	}
 
 	run_bench();
