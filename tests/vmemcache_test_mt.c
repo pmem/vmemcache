a83d323e56ab1b7ff0b2b887cdd4585dc970be0f
Lukasz Dorau <lukasz.dorau@intel.com>
2019-02-01 10:30:26 +0100

    test: fix type of a key
    The type of a key has to be 'unsigned long long'

--- tests/vmemcache_test_mt.c
+++ tests/vmemcache_test_mt.c
@@ -199,7 +199,8 @@ init_test_get(VMEMcache *cache, unsigned n_threads, os_thread_t *threads,
 
 	unsigned n = 0; /* number of elements put into the cache */
 	while (!cache_is_full && n < ops_per_thread) {
-		if (vmemcache_put(cache, &n, sizeof(n),
+		unsigned long long n_key = n;
+		if (vmemcache_put(cache, &n_key, sizeof(n_key),
 					ctx->buffs[n % ctx->nbuffs].buff,
 					ctx->buffs[n % ctx->nbuffs].size))
 			FATAL("ERROR: vmemcache_put: %s", vmemcache_errormsg());
