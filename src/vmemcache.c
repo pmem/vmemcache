7d2faff64b7b1237394ff45b00ffc25574949048
Lukasz Dorau <lukasz.dorau@intel.com>
2019-03-19 10:17:14 +0100

    vmemcache_evict: error out if repl_p_lru_evict fails

--- src/vmemcache.c
+++ src/vmemcache.c
@@ -541,6 +541,9 @@ vmemcache_evict(VMEMcache *cache, const void *key, size_t ksize)
 						&entry->value.p_entry)) {
 			/* release the reference from the replacement policy */
 			vmemcache_entry_release(cache, entry);
+		} else if (cache->repl_p != VMEMCACHE_REPLACEMENT_NONE) {
+			ERR("evicting from LRU list failed");
+			return -1;
 		}
 	}
 
