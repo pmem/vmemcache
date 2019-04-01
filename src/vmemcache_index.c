dbc156d667aad024124c86037ec2c6d438a0b51c
Adam Borowski <adam.borowski@intel.com>
2019-04-01 18:34:34 +0200

    A faster hash for sharding.
    It's fast-hash by Zilong Tan, with my optimizations/simplifications for
    our use case.  No need to care about unaligned access (even on weird archs,
    cache_entries are 8-byte-aligned), no need to pick tail byte-by-byte, etc.
    
    Unlike contenders like MetroHash, no problematic license; and only a few
    lines of the code have been copied.  And, fast-hash is even faster than
    MetroHash.

--- src/vmemcache_index.c
+++ src/vmemcache_index.c
@@ -29,6 +29,28 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+/*
+ * The fast-hash algorithm is covered by the MIT License:
+ *
+ * Copyright (C) 2012 Zilong Tan (eric.zltan@gmail.com)
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 /*
  * vmemcache_index.c -- abstraction layer for vmemcache indexing API
@@ -38,6 +60,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <malloc.h>
+#include <endian.h>
 
 #include "vmemcache.h"
 #include "vmemcache_index.h"
@@ -59,15 +82,39 @@ struct index {
 };
 
 /*
+ * mix -- (internal) helper for the fast-hash mixing step
+ */
+static inline uint64_t
+mix(uint64_t h)
+{
+	h ^= h >> 23;
+	h *= 0x2127599bf4325c37ULL;
+	return h ^ h >> 47;
+}
+
+/*
  * shard_id -- (internal) hash the key and pick a shard bucket id
  */
 static int
 shard_id(size_t key_size, const char *key)
 {
-	/* Fowler–Noll–Vo hash */
-	uint64_t h = 0xcbf29ce484222325;
-	for (size_t i = 0; i < key_size; i++)
-		h = (h ^ (unsigned char)*key++) * 0x100000001b3;
+	/* fast-hash, 2012 Zilong Tan */
+	const uint64_t    m = 0x880355f21e6d1965ULL;
+	const uint64_t *pos = (const uint64_t *)key;
+	const uint64_t *end = pos + (key_size / 8);
+	uint64_t h = key_size * m;
+
+	while (pos != end)
+		h = (h ^ mix(*pos++)) * m;
+
+	if (key_size & 7) {
+		uint64_t shift = (key_size & 7) * 8;
+		uint64_t mask = (1ULL << shift) - 1;
+		uint64_t v = htole64(*pos) & mask;
+		h = (h ^ mix(v)) * m;
+	}
+
+	h = mix(h);
 
 	return h & (NSHARDS - 1);
 }
